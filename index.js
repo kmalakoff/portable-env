ccae00ccfb648f6d3e36a49e53afeca064171526
Kevin Malakoff <kmalakoff@gmail.com>
2019-06-16 16:42:27 -0700

    Tweak

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function isObject(value) {
-  const type = typeof value;
-  return value != null && (type == "object" || type == "function");
-}
-
-function isBrowser() {
-  return typeof window !== "undefined" && isObject(window.__ENV__);
-}
+const isBrowser =
+  typeof window !== "undefined" && typeof window.__ENV__ !== "undefined";
 
 export default function env(key) {
-  return isBrowser() ? window.__ENV__[key] : process.env[key];
+  return isBrowser ? window.__ENV__[key] : process.env[key];
 }
