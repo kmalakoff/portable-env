1fa613a6d244b4ba0d2ea056263937e0daaf3888
Kevin Malakoff <kmalakoff@gmail.com>
2019-06-16 20:50:49 -0700

    Tweak

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const isBrowser =
   typeof window !== "undefined" && typeof window.__ENV__ !== "undefined";
 
-const env = isBrowser ? window.__ENV__ : process.env;
-
 module.exports = function(key) {
+  const env = isBrowser ? window.__ENV__ : process.env;
   return key ? env[key] : env;
 };
