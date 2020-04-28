ad752935d058557b3dc463b09cbfa9d488110146
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-28 08:54:03 -0700

    Added load method and tests

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var isBrowser = typeof window !== 'undefined' && typeof window.__ENV__ !== 'undefined';
-var env = isBrowser ? window.__ENV__ : process.env;
+var env = null;
 
-module.exports = function () {
+module.exports = function portableEnv() {
   return env;
 };
+
+module.exports.load = function load() {
+  var isBrowser = typeof window !== 'undefined' && typeof window.__ENV__ !== 'undefined';
+  env = isBrowser ? window.__ENV__ : process.env;
+};
+module.exports.load();
