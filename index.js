5e81945a9be0ab96c5c9a9dd99550e969ee0f8f2
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-28 08:38:58 -0700

    Lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var isBrowser = typeof window !== 'undefined' && typeof window.__ENV__ !== 'undefined';
 var env = isBrowser ? window.__ENV__ : process.env;
 
-module.exports = function() {
+module.exports = function () {
   return env;
 };
