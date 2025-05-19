29036e706c5a356e06891820218c61f519f0fd88
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 13:14:37 -0700

    Upgrade to mandatory typescript extensions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ export default function portableEnv() {
   return env;
 }
 
+declare global {
+  interface Window {
+    __ENV__: NodeJS.ProcessEnv;
+  }
+}
+
 export function load() {
   const isBrowser = typeof window !== 'undefined' && typeof window.__ENV__ !== 'undefined';
   env = isBrowser ? window.__ENV__ : process.env;
