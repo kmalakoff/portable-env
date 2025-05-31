5895a507b1698e77ebf468586642347ae32b9304
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 11:06:36 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 let env = null;
 
-export default function portableEnv() {
-  return env;
-}
-
 declare global {
   interface Window {
     __ENV__: NodeJS.ProcessEnv;
   }
 }
 
-export function load() {
+export default (): NodeJS.ProcessEnv => env;
+export function load(): NodeJS.ProcessEnv {
   const isBrowser = typeof window !== 'undefined' && typeof window.__ENV__ !== 'undefined';
   env = isBrowser ? window.__ENV__ : process.env;
+  return env;
 }
 load();
